417d334dc05721cf91a33cf78369945db648d08e
SayedTahsin <mail.tahsin99@gmail.com>
2024-08-07 23:47:32 +0600

    updated response
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- pages/courses/[course]/index.vue
+++ pages/courses/[course]/index.vue
@@ -1,18 +1,59 @@
 <script setup lang="ts">
 import axios from 'axios'
 
+interface Course {
+  title: string
+  description: string
+  instructors: Array<string>
+  moderators: Array<string>
+  startDate: string
+  endDate: string
+  duration: number
+  capacity: number
+  students: number
+  price: number
+  image: string
+  rating: number
+}
+interface Lesson {
+  title: string
+}
 const router = useRouter()
 const route = useRoute()
-const courseName = route.params.course
+const courseId = route.params.course
 const ifContentHidden = ref(false)
+const courseObject: Ref<Course | null> = ref({
+  title: 'Go: The Complete Developer\'s Guide (Golang)',
+  description: 'Master the fundamentals and advanced features of the Go Programming Language (Golang)',
+  instructors: [
+    'sayedtahsin',
+  ],
+  moderators: [],
+  startDate: '15th August',
+  endDate: '',
+  duration: 3,
+  capacity: 0,
+  students: 0,
+  price: 10000,
+  rating: 4.6,
+  image: 'https://miro.medium.com/v2/resize:fit:1024/1*V8JWIC-tqYQkS1b1edsu3w.png',
+})
+const Lessons: Ref<Array<Lesson>> = ref([
+  {
+    title: 'Introduction',
+  },
+  {
+    title: 'Concurrency',
+  },
+])
 async function getCourse() {
-  const url = `http://localhost:3030/course/${courseName}`
+  const url = `http://localhost:3030/course/${courseId}`
   const resp = await axios.get(url)
+  courseObject.value = resp.data
   return resp
 }
-
-async function getContent() {
-  const url = `http://localhost:3030/course/${courseName}{/content/list`
+async function getLesson() {
+  const url = `http://localhost:3030/course/${courseId}/lesson/list`
   const resp = await axios.get(url)
   return resp
 }
@@ -41,40 +82,67 @@ const contentTable = [{
 
 onMounted(() => {
   getCourse()
-  getContent()
+  getLesson()
 })
 </script>
 
 <template>
   <div class="px-2 py-5 sm:py-9 max-w-6xl mt-10 mx-auto">
     <div class="flex justify-center mb-6">
-      <img class="h-40 w-full" src="https://miro.medium.com/v2/resize:fit:1024/1*V8JWIC-tqYQkS1b1edsu3w.png">
+      <img class="h-40 w-full" :src="courseObject?.image">
     </div>
     <div class="container mx-auto p-4 sm:grid sm:grid-cols-3 sm:gap-4">
       <div class="sm:col-span-2">
         <div class="mb-4">
           <h1 class="text-3xl font-bold">
-            Kubernetes Basic to Advance
+            {{ courseObject?.title }}
           </h1>
         </div>
         <div class="mb-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eaque libero maxime accusantium dolorum repellat dolor minima cumque unde numquam, asperiores, reiciendis, ad necessitatibus officiis similique perspiciatis dolore temporibus sed?
+          {{ courseObject?.description }}
+        </div>
+        <br>
+        <div class="flex justify-between">
+          <div class="font-bold mb-2">
+            Rating: {{ courseObject?.rating }} ⭐
+          </div>
+          <div class="font-bold mb-4">
+            Students Enrolled: {{ courseObject?.students }}
+          </div>
+        </div>
+        <div class="flex justify-between">
+          <div class="font-bold mb-2">
+            Start Date: {{ courseObject?.startDate }}
+          </div>
+          <div class="font-bold mb-4">
+            Duration: {{ courseObject?.duration }} hours
+          </div>
         </div>
         <div class="font-bold mb-2">
-          ⭐Rating: 5.0
+          Price: {{ courseObject?.price }} tk
         </div>
-        <div class="font-bold mb-4">
-          🌐Language: English
-        </div>
+        <br>
         <p class="text-2xl font-bold mb-1">
-          Instructor
+          Instructors
         </p>
-        <div class="flex items-center space-x-4 mb-4">
+        <div v-for="instructor in courseObject?.instructors" :key="instructor" class="flex items-center space-x-4 mb-4">
           <div>
             <img class="h-20 w-20 rounded-full border-2 border-gray-500" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR__2IIAULCR-xberpmuxf-9Jx3cJZLJgLm4tSb9cDwRQ&s">
           </div>
           <div>
-            <p>Name : Tahsin</p>
+            <p>Name : {{ instructor }}</p>
+            <p>Company : Appscode</p>
+          </div>
+        </div>
+        <p class="text-xl font-bold mb-1">
+          Moderators
+        </p>
+        <div v-for="instructor in courseObject?.instructors" :key="instructor" class="flex items-center space-x-4 mb-4">
+          <div>
+            <img class="h-10 w-10 rounded-full border-2 border-gray-500" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR__2IIAULCR-xberpmuxf-9Jx3cJZLJgLm4tSb9cDwRQ&s">
+          </div>
+          <div class="text-xs">
+            <p>Name : {{ instructor }}</p>
             <p>Company : Appscode</p>
           </div>
         </div>
@@ -113,16 +181,13 @@ onMounted(() => {
               </div>
             </div>
             <div v-if="!ifContentHidden" class="border border-t-0 border-gray-300 h-36 overflow-y-auto">
-              <div v-for="content in contentTable" :key="content.id" @click="router.push(`${courseName}/${content.title}`)">
+              <div v-for="lesson in Lessons" :key="lesson.title" @click="router.push(`${courseId}/${lesson.title}`)">
                 <div class="flex items-center mb-2 cursor-pointer">
                   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" class="fill-current text-gray-600">
                     <path d="m9.5 16.5l7-4.5l-7-4.5zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" />
                   </svg>
                   <div class="ml-2">
-                    <p>{{ content.title }}</p>
-                    <p class="text-sm text-gray-500">
-                      {{ content.time }}
-                    </p>
+                    <p>{{ lesson.title }}</p>
                   </div>
                 </div>
               </div>
@@ -146,16 +211,13 @@ onMounted(() => {
             </div>
           </div>
           <div v-if="!ifContentHidden" class="border border-t-0 border-gray-300 p-3">
-            <div v-for="content in contentTable" :key="content.id" @click="router.push(`${courseName}/${content.title}`)">
+            <div v-for="lesson in Lessons" :key="lesson.title" @click="router.push(`${courseId}/${lesson.title}`)">
               <div class="flex items-center mb-2 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" class="fill-current text-gray-600">
                   <path d="m9.5 16.5l7-4.5l-7-4.5zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" />
                 </svg>
                 <div class="ml-2">
-                  <p>{{ content.title }}</p>
-                  <p class="text-sm text-gray-500">
-                    {{ content.time }}
-                  </p>
+                  <p>{{ lesson.title }}</p>
                 </div>
               </div>
             </div>
